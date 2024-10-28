d57900afa8a16b4faf4703fb769ffddeada3db47
Agustina <agustinaayelensosa@gmail.com>
2024-10-28 14:02:30 -0300

    feat: componente acordeon

--- shadcd-dashboard/src/app/dashboard/accordion/page.tsx
+++ shadcd-dashboard/src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,37 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function Page() {
   return (
     <div>
-      <h1>Accordion Page</h1>
+      {/* para dejar que te deje varias opciones abiertas hay que cambiar el type a multiple y sacar el collapsible */}
+      <Accordion type="single" collapsible className="w-full">
+        {/* para mapear un array solo hay que envolver AccordionItem dentro del map  */}
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other
+            components&apos; aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It&apos;s animated by default, but you can disable it if you
+            prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
